dcfa78f3b44ba44d44c4de86b05e1ac1cab62be5
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2026-04-28 17:02:25 -0300

    Teste de Camera

---
+++ OficinaJogos/Assets/Scripts/Player Scripts/Movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+   
+    
+    public float speed;
+    
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+            {
+               rb.linearVelocity = new Vector2(rb.linearVelocity.x, speed);
+            }
+    }
+}
