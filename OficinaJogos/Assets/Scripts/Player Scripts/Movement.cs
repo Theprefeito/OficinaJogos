94d5288df980060b7f78b09f0a03afa250fa1447
FernandoSantos0920 <fernando.santos2@escolar.ifrn.edu.br>
2026-05-05 17:38:05 -0300

    Atualizar o movimento

--- OficinaJogos/Assets/Scripts/Player Scripts/Movement.cs
+++ OficinaJogos/Assets/Scripts/Player Scripts/Movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -6,16 +7,31 @@ public class Movement : MonoBehaviour
     
     public float speed;
     
-    private Rigidbody2D rb;
+    private Rigidbody rb;
+    
+    private Vector2 direction;
+
+    public Transform camPosition;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 mover = new Vector3(direction.x, 0, direction.y);
         
+        rb.MovePosition(rb.transform.position + mover * speed * Time.fixedDeltaTime);
+        
+        // Olha pra cam
+        rb.transform.rotation = Quaternion.Euler(0, camPosition.rotation.eulerAngles.y, 0);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
     }
 }
