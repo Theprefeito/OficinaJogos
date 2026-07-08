391c20d7aca88b7467a551e6392081cceb2b22cc
José Kleyton da Silva Pacheco <kleyton.j@escolar.ifrn.edu.br>
2026-07-08 14:22:26 -0300

    dash1

---
+++ OficinaJogos/Assets/Scenes/playerMoviment.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class playerMoviment : MonoBehaviour
+{
+  /* private dash dash;
+    
+   void Start()
+   {
+      dash = GetComponent<dash>();
+   }
+
+   private void Update()
+   {
+      if 
+         //(imput.getkey)
+      {
+         
+      }
+   }*/
+}
---
+++ OficinaJogos/Assets/Scripts/dash/dash.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class dash : MonoBehaviour
+{
+   /* public float veocidade = 20f;
+    public float duração = 0.2f;
+    
+    private Rigidbody2D rb;
+    private bool usandoDash;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void ExecutarDash()
+    {
+        if (!usandoDash) 
+            
+  //          StartCoroutine(dashCoroutine());
+    }
+   // IEnumerator dashCoroutine()
+    {
+  //      usandoDash = true;
+ //       rb.linearVelocity = transform.up * veocidade;
+
+  //      yield return new WaitForSeconds(duração);
+  //      rb.linearVelocity = Vector3.zero;
+  //      usandoDash = false;
+        
+    }*/
+}
