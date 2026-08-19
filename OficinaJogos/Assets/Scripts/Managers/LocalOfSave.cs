1e02ebb607ee13f9048cb354cdf34d57ce1afdd3
Fernando Santos da Silva <fernando.santos2@escolar.ifrn.edu.br>
2026-08-19 14:26:11 -0300

    Posição global quase Ok

---
+++ OficinaJogos/Assets/Scripts/Managers/LocalOfSave.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class LocalOfSave : MonoBehaviour
+{
+   
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+    
+
+   void OnTriggerEnter(Collider collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+       
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Bateu");
+                    
+            PositionManager.Instance.position = collision.transform.position;
+            PositionManager.Instance.rotation = collision.transform.rotation;
+        
+            PositionManager.Instance.SavedPosition = true;
+        }
+    }
+    
+    
+    
+}
---
+++ OficinaJogos/Assets/Scripts/Managers/PositionManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PositionManager : MonoBehaviour
+{
+    [Header("Position")]
+    public static PositionManager Instance;
+
+    public Vector3 position;
+    public Quaternion rotation;
+
+    [Header("Scene")]
+    
+    [SerializeField] public int scene;
+    
+    public bool SavedPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
