0ffdb482321504a62ea79742f8a10adc73b744af
Fernando Santos da Silva <fernando.santos2@escolar.ifrn.edu.br>
2026-09-16 14:28:26 -0300

    Inimigo patrulheiro quase pronto

---
+++ OficinaJogos/Assets/Scripts/Enemys/PatrolEnemy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PatrolEnemy : MonoBehaviour
+{
+   
+    [SerializeField] private Transform[] waypoints;
+    public int wayIndex;
+    public float speedEnemy;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        wayIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[wayIndex].position, speedEnemy * Time.deltaTime);
+       
+        if (transform.position == waypoints[wayIndex].position)
+        {
+            wayIndex++;
+        }
+
+        if (wayIndex >= waypoints.Length)
+        {
+            wayIndex = 0;
+        }
+    }
+}
