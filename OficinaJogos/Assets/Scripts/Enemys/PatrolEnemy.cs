a1db7d9e87e667b8adfd2b885624031f6c08b2a3
Fernando Santos da Silva <fernando.santos2@escolar.ifrn.edu.br>
2026-09-25 10:23:33 -0300

    Physics adicionada ao inimigo de patrulha

--- OficinaJogos/Assets/Scripts/Enemys/PatrolEnemy.cs
+++ OficinaJogos/Assets/Scripts/Enemys/PatrolEnemy.cs
@@ -1,24 +1,53 @@
+using System.Collections;
 using UnityEngine;
 
 public class PatrolEnemy : MonoBehaviour
 {
-   
+    Rigidbody rbEnemy;
+    [Header("Points To Patrol")]
     [SerializeField] private Transform[] waypoints;
+
+   
     public int wayIndex;
-    public float speedEnemy;
-    
+    [Header("Values for patrol")]
+    [SerializeField] public float distanceToWaypoint;
+    [Header("Enemy Stats")]
+    [SerializeField] public float speedEnemy;
+
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   
+        rbEnemy =  GetComponent<Rigidbody>();
         wayIndex = 0;
     }
 
+    void FixedUpdate()
+    {
+
+      
+        {
+            EnemyPatrolMovement();
+                    
+            WaypointPatrol();
+        }
+        
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[wayIndex].position, speedEnemy * Time.deltaTime);
-       
-        if (transform.position == waypoints[wayIndex].position)
+    
+    }
+
+    private void EnemyPatrolMovement()
+    {
+        Vector3 posicao = Vector3.MoveTowards(transform.position, waypoints[wayIndex].position, speedEnemy * Time.fixedDeltaTime);
+        rbEnemy.MovePosition(posicao);
+    }
+
+    private void WaypointPatrol()
+    {
+        if (Vector3.Distance(transform.position, waypoints[wayIndex].position) <= distanceToWaypoint)
         {
             wayIndex += 1;
 
@@ -26,12 +55,8 @@ public class PatrolEnemy : MonoBehaviour
             {
                 wayIndex = 0;
             }
+            
         }
-
-    
     }
-
-    
-    
-    
+   
 }
